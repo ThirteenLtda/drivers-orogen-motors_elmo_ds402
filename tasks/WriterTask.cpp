8303ea975876c805919556ff17c4978f4f0f2170
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-08-06 19:05:06 -0300

    implement te WriterTask

--- tasks/WriterTask.cpp
+++ tasks/WriterTask.cpp
@@ -1,106 +1,111 @@
 /* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
 
-#include "ReaderTask.hpp"
+#include "WriterTask.hpp"
 #include <canopen_master/SDO.hpp>
 
 using namespace motors_elmo_ds402;
 
-ReaderTask::ReaderTask(std::string const& name)
-    : ReaderTaskBase(name)
+WriterTask::WriterTask(std::string const& name)
+    : WriterTaskBase(name)
     , mController(0)
 {
-    mJoints.elements.resize(1);
 }
 
-ReaderTask::ReaderTask(std::string const& name, RTT::ExecutionEngine* engine)
-    : ReaderTaskBase(name, engine)
+WriterTask::WriterTask(std::string const& name, RTT::ExecutionEngine* engine)
+    : WriterTaskBase(name, engine)
     , mController(0)
 {
-    mJoints.elements.resize(1);
 }
 
-ReaderTask::~ReaderTask()
+WriterTask::~WriterTask()
 {
 }
 
 
 
 /// The following lines are template definitions for the various state machine
-// hooks defined by Orocos::RTT. See ReaderTask.hpp for more detailed
+// hooks defined by Orocos::RTT. See WriterTask.hpp for more detailed
 // documentation about them.
 
-bool ReaderTask::configureHook()
+bool WriterTask::configureHook()
 {
-    if (! ReaderTaskBase::configureHook())
+    if (! WriterTaskBase::configureHook())
         return false;
 
     mController = Controller(_can_id.get());
-    mExpectedJointState = 0;
-    auto expected_joint_state = _required_joint_states.get();
-    if (expected_joint_state.position)
-        mExpectedJointState |= UPDATE_JOINT_POSITION;
-    if (expected_joint_state.velocity)
-        mExpectedJointState |= UPDATE_JOINT_VELOCITY;
-    if (expected_joint_state.current_and_torque)
-        mExpectedJointState |= UPDATE_JOINT_CURRENT;
-
     auto queryFactors = mController.queryFactors();
     if (!readSDOs(queryFactors, UPDATE_FACTORS, base::Time::fromSeconds(2)))
         return false;
     mController.setMotorParameters(_motor_parameters.get());
 
+    mJoints.resize(1);
+
     _can_out.write(mController.queryNodeStateTransition(
         canopen_master::NODE_ENTER_PRE_OPERATIONAL));
-    canopen_master::PDOCommunicationParameters parameters;
-    auto pdoSetup = mController.configureJointStateUpdatePDOs(
-        2, _tpdo_configuration.get(), mExpectedJointState);
+    writeSDO(mController.send(ControlWord(ControlWord::SHUTDOWN, true)));
+
+    switch(_control_mode.get())
+    {
+        case base::JointState::POSITION:
+            writeSDO(mController.setOperationMode(OPERATION_MODE_CYCLIC_SYNCHRONOUS_POSITION));
+            break;
+        case base::JointState::SPEED:
+            writeSDO(mController.setOperationMode(OPERATION_MODE_CYCLIC_SYNCHRONOUS_VELOCITY));
+            break;
+        case base::JointState::EFFORT:
+            writeSDO(mController.setOperationMode(OPERATION_MODE_PROFILED_TORQUE));
+            break;
+        default:
+            throw std::invalid_argument("invalid control mode");
+    }
+    auto pdoSetup = mController.configureControlPDO(
+        0, _control_mode.get(), _rpdo_configuration.get());
     if (!writeSDOs(pdoSetup, base::Time::fromSeconds(2)))
         return false;
     _can_out.write(mController.queryNodeStateTransition(
         canopen_master::NODE_START));
     return true;
 }
-
-bool ReaderTask::startHook()
+bool WriterTask::startHook()
 {
-    if (! ReaderTaskBase::startHook())
+    if (! WriterTaskBase::startHook())
         return false;
-    mUpdate = Update();
-    mJoints.time = base::Time();
-    mJoints.elements[0] = base::JointState();
+
+    writeSDO(mController.send(ControlWord(ControlWord::SWITCH_ON, true)));
+    writeSDO(mController.send(ControlWord(ControlWord::ENABLE_OPERATION, false)));
     return true;
 }
-void ReaderTask::updateHook()
+void WriterTask::updateHook()
 {
-    canbus::Message msg;
-    while (_can_in.read(msg, false) == RTT::NewData)
+    WriterTaskBase::updateHook();
+
+    while (_joint_command.read(mJoints, false) == RTT::NewData)
     {
-        mUpdate.merge(mController.process(msg));
-        if (mUpdate.isUpdated(mExpectedJointState)) {
-            base::JointState state = mController.getJointState(mExpectedJointState);
-            mJoints.time = msg.time;
-            mJoints.elements[0] = state;
-            _joints_state_samples.write(mJoints);
-            mUpdate = Update();
-        }
+        if (mJoints.size() != 1)
+            return exception(INVALID_COMMAND);
+        base::JointState const& state = mJoints.elements[0];
+        if (base::isUnset(state.getField(_control_mode.get())))
+            return exception(INVALID_COMMAND);
+
+        mController.setControlTargets(mJoints.elements[0]);
+        _can_out.write(mController.getRPDOMessage(0));
     }
+}
+void WriterTask::errorHook()
+{
+    WriterTaskBase::errorHook();
+}
+void WriterTask::stopHook()
+{
+    writeSDO(mController.send(ControlWord(ControlWord::SHUTDOWN, true)));
+    WriterTaskBase::stopHook();
+}
+void WriterTask::cleanupHook()
+{
+    WriterTaskBase::cleanupHook();
+}
 
-    ReaderTaskBase::updateHook();
-}
-void ReaderTask::errorHook()
-{
-    ReaderTaskBase::errorHook();
-}
-void ReaderTask::stopHook()
-{
-    ReaderTaskBase::stopHook();
-}
-void ReaderTask::cleanupHook()
-{
-    ReaderTaskBase::cleanupHook();
-}
-
-bool ReaderTask::readSDOs(std::vector<canbus::Message> const& queries,
+bool WriterTask::readSDOs(std::vector<canbus::Message> const& queries,
     int expectedUpdate, base::Time timeout)
 {
     for (auto const& query : queries) {
@@ -109,7 +114,7 @@ bool ReaderTask::readSDOs(std::vector<canbus::Message> const& queries,
     }
     return true;
 }
-bool ReaderTask::readSDO(canbus::Message const& query,
+bool WriterTask::readSDO(canbus::Message const& query,
     int expectedUpdate, base::Time timeout)
 {
     _can_out.write(query);
@@ -133,7 +138,7 @@ bool ReaderTask::readSDO(canbus::Message const& query,
     return false;
 }
 
-bool ReaderTask::writeSDOs(std::vector<canbus::Message> const& queries,
+bool WriterTask::writeSDOs(std::vector<canbus::Message> const& queries,
     base::Time timeout)
 {
     for (auto const& query : queries) {
@@ -142,7 +147,7 @@ bool ReaderTask::writeSDOs(std::vector<canbus::Message> const& queries,
     }
     return true;
 }
-bool ReaderTask::writeSDO(canbus::Message const& query, base::Time timeout)
+bool WriterTask::writeSDO(canbus::Message const& query, base::Time timeout)
 {
     _can_out.write(query);
 
